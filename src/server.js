d0596f99a1326c467bf9ea24ffa810a5dc493740
Karthick R <karthick.r@zohocorp.com>
2019-08-02 17:31:55 +0530

    Post and Get routes

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const journalSchema = require('../model/journal');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -11,6 +12,7 @@ const rateLimiter = rateLimit({
 });
 
 app.use(rateLimiter);
+app.use(express.json());
 
 // Starting server in port 4000 
 app.listen(port, () => {
@@ -19,4 +21,29 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.send(`Welcome to Revista...`);
+});
+
+let id = 0;
+let journalArr = new Array();
+
+app.post('/api/addNote', (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let journal = journalSchema;
+    journal.title = title;
+    journal.body = body;
+    journal.id = id+1;
+    journalArr.push(journal);
+    res.send(journal);
+});
+
+app.get('/api/getNote/:id', (req, res) => {
+    for (let i = 0; i < journalArr.length; i++) {
+        if (req.params.id == i + 1) {
+            let journalResponse = journalArr[i];
+            res.send(journalResponse);
+            return;
+        }
+    }
+    res.send('Not found');
 });
