c813c5063d65a0261c32e4f98e7a28e57444130c
karthickram286 <karthick.r@zohocorp.com>
2019-08-14 22:33:58 +0530

    unhandled promise rejection handling

--- server.js
+++ server.js
@@ -8,8 +8,15 @@ const listRoutes = require('./routes/listRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dotenv = require('dotenv').config();
 
+// For Synchronous errors anywhere inside the application
 process.on('uncaughtException', (ex) => {
     console.log('Uncaught Exception...' + ex.message);
+    winstonLogger.error(ex.message, ex);
+});
+
+// For ASynchronous errors(Promise rejections) anywhere inside the application
+process.on('unhandledRejection', (ex) => {
+    console.log('Unhandled Promise Rejection...' + ex.message);
     winstonLogger.error(ex.message, ex);
 });
 
