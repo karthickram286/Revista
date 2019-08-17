345546b688fa187b640fac2c4a9eb3c4bc59c51e
karthickram286 <karthick.r@zohocorp.com>
2019-08-17 12:16:00 +0530

    notes test get request

--- tests/integration/notes.test.js
+++ tests/integration/notes.test.js
@@ -2,6 +2,7 @@ const superTest = require('supertest');
 const User = require('../../model/user');
 let server;
 let token;
+let noteId;
 
 describe('User Rest APIs', () => {
 
@@ -15,7 +16,6 @@ describe('User Rest APIs', () => {
 
     describe('POST /', () => {
 
-        let noteId;
         it('should add a note', async () => {
             const res = await superTest(server)
                 .post('/api/notes/addNote')
@@ -26,12 +26,30 @@ describe('User Rest APIs', () => {
             expect(res.status).toBe(200);
         });
 
-        it('should update a note', async () => {
+        it('should update a note for a given id', async () => {
             const res = await superTest(server)
                 .post('/api/notes/updateNote/' + noteId)
                 .set('x-auth-token', token)
                 .send({ title: 'updated-test-title', body: 'updated lorem ipsum' });
 
+            expect(res.status).toBe(200);
+        });
+    });
+
+    describe('Get /', () => {
+        it('should get a note for a given id', async () => {
+            const res = await superTest(server)
+                .get('/api/notes/getNote/' + noteId)
+                .set('x-auth-token', token);
+            
+            expect(res.status).toBe(200);
+        });
+
+        it ('should return all notes', async () => {
+            const res = await superTest(server)
+                .get('/api/notes/getAllNotes')
+                .set('x-auth-token', token);
+            
             expect(res.status).toBe(200);
         });
     });
