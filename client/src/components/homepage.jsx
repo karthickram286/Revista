5d3a51a5c07b018956facb749ae5c5dc32c412f7
karthickram286 <karthick.r@zohocorp.com>
2019-08-21 10:32:38 +0530

    homepage changes

--- client/src/components/homepage.jsx
+++ client/src/components/homepage.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 import revistaLogo from './styles/revista-logo.png'
 import '../App.css'
 
-const HomePage = (props) => {
-    let userName = props.name;
-    if (userName !== undefined) {
+const HomePage = () => {
+    let authToken = document.cookie.authToken;
+    if (authToken !== undefined) {
         return (
             <div className="homepage">
-                <h3>Welcome { userName }</h3>
-                <Link to="/login" style={{marginRight: 15}} className="btn btn-primary">Login</Link>
-                <Link to="/register" className="btn btn-light">Sign Up</Link>
+                <h3>Welcome to Revista</h3>
+                <Link to="/logout" style={{marginRight: 15}} className="btn btn-primary">Logout</Link>
+                <Link to="/notes" className="btn btn-light">Notes</Link>
             </div>
         );
     } else {
--- client/src/components/note.component.jsx
+++ client/src/components/note.component.jsx
@@ -4,7 +4,7 @@ export const NoteComponent = (props) => {
     return (
         <div className="note">
             <h3 key={ props.note.id }>{ props.note.title }</h3>
-            <h5 key={ props.note.id }>{ props.note.boyd }</h5>
+            <h5 key={ props.note.id }>{ props.note.body }</h5>
         </div>
     );
 }
