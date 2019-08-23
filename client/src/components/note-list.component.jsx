ad963d95555f32f27e64215d40b93ee85beac131
karthickram286 <karthick.r@zohocorp.com>
2019-08-23 12:57:06 +0530

    notes styling issues

--- client/src/components/note-list.component.jsx
+++ client/src/components/note-list.component.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 import { NoteComponent } from './note.component';
 
 import './styles/note-list.component.css'
-import { CardDeck } from 'react-bootstrap';
 
 export const NoteList = (props) => {
     return(
         <div className="notes">
-            <CardDeck>
-                {
-                    props.notes.map(note => (
-                        <NoteComponent key={ note._id } note={ note } />
-                    ))
-                }
-            </CardDeck>
+            {
+                props.notes.map(note => (
+                    <NoteComponent key={ note._id } note={ note } />
+                ))
+            }
         </div>
     );
 }
