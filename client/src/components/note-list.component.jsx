a2047419bca8105a354634bfbb0b0286f38e2ea7
karthickram286 <karthick.r@zohocorp.com>
2019-08-23 12:39:48 +0530

    notes styling

--- client/src/components/note-list.component.jsx
+++ client/src/components/note-list.component.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import { NoteComponent } from './note.component';
 
 import './styles/note-list.component.css'
+import { CardDeck } from 'react-bootstrap';
 
 export const NoteList = (props) => {
     return(
-        <div className="note-list">
-            {
-                props.notes.map(note => (
-                    <NoteComponent key={ note._id } note={ note } />
-                ))
-            }
+        <div className="notes">
+            <CardDeck>
+                {
+                    props.notes.map(note => (
+                        <NoteComponent key={ note._id } note={ note } />
+                    ))
+                }
+            </CardDeck>
         </div>
     );
 }
--- client/src/components/note.component.jsx
+++ client/src/components/note.component.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 
 import './styles/note.component.css';
-import Footer from './footer';
 
 export const NoteComponent = (props) => {
     console.log(props.note);
     return (
         <div className="noteCard">
-            <Card style={{ width: '18rem' }}>
-                <Card.Header as="h4" bg="info">{ props.note.title }</Card.Header>
+            <Card bg="info">
+                <Card.Header as="h4">{ props.note.title }</Card.Header>
                 <Card.Body>
                     <Card.Text>{ props.note.body }</Card.Text>
                 </Card.Body>
                 <Card.Footer>
-                    <small className="text-muted">{ props.note.dateCreated }</small>
-                    <small className="text-muted">{ props.note.lastModified }</small>
+                    <small className="text-muted">Date Created: { props.note.dateCreated }</small>
+                </Card.Footer>
+                <Card.Footer>
+                    <small className="text-muted">Last Modified: { props.note.lastModified }</small>
                 </Card.Footer>
             </Card>
         </div>
